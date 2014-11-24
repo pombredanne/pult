0d1f228d95e4202a7fb28c205fead15f3db69bca
Timo Sulg <timgluz@gmail.com>
2014-11-24 18:23:27 +0200

    keep directional buttons down until touch ends;

--- src/pult/core.cljs
+++ src/pult/core.cljs
@@ -6,9 +6,10 @@
 
 (def app-state (atom {:uri "ws://127.0.0.1:8080/ws"
                       :connection nil
-                      :incoming-ch (async/chan 10)
-                      :outgoing-ch (async/chan (async/sliding-buffer 10))
+                      :incoming-ch (async/chan 5)
+                      :outgoing-ch (async/chan (async/sliding-buffer 5))
                       :event-ch (async/chan 10)
+                      :freq 10 ;how often generate impulses
                       :mappings {"btn-up" :UP
                                  "btn-right" :RIGHT
                                  "btn-left" :LEFT
@@ -133,23 +134,51 @@
       (.error js/console
               (str "Failed to register event for:" selector " , " action-name)))))
 
+(defmulti event->action
+  (fn [ev] (.-type ev)))
+
+(defmethod event->action "click" [ev]
+  {:action :key-press
+   :duration 30
+   :release? true})
+
+(defmethod event->action "touchstart" [ev]
+  {:action :key-press
+   :release? false})
+
+(defmethod event->action "touchend" [ev]
+  {:action :key-release})
+
+(defmethod event->action "mousedown" [ev]
+  {:action :key-press
+   :release? false})
+
+(defmethod event->action "mouseup" [ev]
+  {:action :key-release})
+
+(defmethod event->action :default [ev]
+  (.error js/console "Unknown event - cant translate it to action." ev))
+
 (defn send-actions
   "listen controller actions on event channel and sends it to the server"
-  [outgoing-ch event-feed action-id]
+  [outgoing-ch event-feed source-id]
   (let [ctrl-ch (async/chan (async/sliding-buffer 5))]
-    (async/sub event-feed action-id ctrl-ch)
+    (async/sub event-feed source-id ctrl-ch)
     (.debug js/console "Listening controller events ...")
     (go-loop []
-      (when-let [action (<! ctrl-ch)]
-        (let [btn-id (.. (:event action) -target -id)
+      (when-let [dt (<! ctrl-ch)]
+        (let [duration (:duration @app-state)
+              btn-id (.. (:event dt) -target -id)
               btn-code (get-in @app-state [:mappings btn-id] :not-mapped)]
-          (async/put! outgoing-ch {:id :command
-                                   :action action-id
-                                   :key btn-code
-                                   :duration 100
-                                   :delay 5
-                                   :start (current-time)})
-          (vibrate! 10)
+          (.debug js/console (:event dt))
+          (some->>
+                (:event dt)
+                event->action
+                (merge {:id :command
+                        :key btn-code
+                        :start (current-time)})
+                (async/put! outgoing-ch))
+          (vibrate! duration)
           (recur))))))
 
 (defn ^:export main
@@ -159,10 +188,10 @@
         incoming-ch (:incoming-ch @app-state) ;(async/chan 10)
         outgoing-ch (:outgoing-ch @app-state) ;(async/chan (async/sliding-buffer 10))
         event-ch (:event-ch @app-state)
-        event-feed (async/pub event-ch :action)
-        action-fn (fn [ev]
+        event-feed (async/pub event-ch :source)
+        into-feed (fn [ev]
                    (async/put! event-ch
-                               {:action :key-press
+                               {:source :controller
                                 :event ev}))]
     (if (nil? svg-doc)
       (.error js/console "Failed to load controller UI.")
@@ -174,19 +203,37 @@
                            on-connect)
         ;;-- register controller events
         (register-events! svg-doc
-                          [["btn-up"    "click" action-fn]
-                           ["btn-down"  "click" action-fn]
-                           ["btn-left"  "click" action-fn]
-                           ["btn-right" "click" action-fn]
-                           ["btn-a"     "click" action-fn]
-                           ["btn-b"     "click" action-fn]
-                           ["btn-select" "click" action-fn]
-                           ["btn-start" "click" action-fn]
-                           ["btn-configure" "click" action-fn]
-                           ["btn-connect" "click" action-fn]])
+                          [
+                           ["btn-up"    "touchstart"  into-feed]
+                           ["btn-up"    "touchend"    into-feed]
+                           ["btn-up"    "mousedown"   into-feed]
+                           ["btn-up"    "mouseup"     into-feed]
+
+                           ["btn-down"  "touchstart"  into-feed]
+                           ["btn-down"  "touchend"    into-feed]
+                           ["btn-down"  "mousedown"   into-feed]
+                           ["btn-down"  "mouseup"     into-feed]
+
+                           ["btn-left"  "touchstart"  into-feed]
+                           ["btn-left"  "touchend"    into-feed]
+                           ["btn-left"  "mousedown"   into-feed]
+                           ["btn-left"  "mouseup"     into-feed]
+
+                           ["btn-right" "touchstart"  into-feed]
+                           ["btn-right" "touchend"    into-feed]
+                           ["btn-right" "mousedown"   into-feed]
+                           ["btn-right" "mouseup"     into-feed]
+
+                           ;;buttons with single impulse
+                           ["btn-a"     "click"       into-feed]
+                           ["btn-b"     "click"       into-feed]
+                           ["btn-select" "click"      into-feed]
+                           ["btn-start" "click"       into-feed]
+                           ["btn-configure" "click"   into-feed]
+                           ["btn-connect" "click"     into-feed]])
 
         ;;-- push controller cought action to the server
-        (send-actions outgoing-ch event-feed :key-press)
+        (send-actions outgoing-ch event-feed :controller)
         ))))
 
 (main)
